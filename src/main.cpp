1525fc3f650cb04069ac79e5abda525512e48f60
ryt1s <rytisj27@gmail.com>
2025-12-16 07:03:52 +0200

    Count repeated words using map and write results to file

--- src/main.cpp
+++ src/main.cpp
@@ -3,35 +3,45 @@
 #include <sstream>
 #include <string>
 #include <cctype>
+#include <map>
 
 using namespace std;
 
 int main() {
     ifstream in("data/input.txt");
     if (!in) {
-        cerr << "Nepavyko atidaryti input.txt" << endl;
+        cerr << "Nepavyko atidaryti data/input.txt" << endl;
         return 1;
     }
 
-    string line;
-    int lineNumber = 0;
+    map<string, int> wordCount;
 
+    string line;
     while (getline(in, line)) {
-        ++lineNumber;
-        
         for (char& c : line) {
-            if (!isalnum(static_cast<unsigned char>(c))) {
-                c = ' ';
-            }
+            if (!isalnum(static_cast<unsigned char>(c))) c = ' ';
         }
 
         stringstream ss(line);
         string word;
 
         while (ss >> word) {
-            cout << "[" << word << "] (eilute " << lineNumber << ")" << endl;
+            wordCount[word]++;
         }
     }
 
+    ofstream out("outputs/words.txt");
+    if (!out) {
+        cerr << "Nepavyko sukurti outputs/words.txt" << endl;
+        return 1;
+    }
+
+    for (const auto& p : wordCount) {
+        if (p.second > 1) {
+            out << p.first << " : " << p.second << "\n";
+        }
+    }
+
+    cout << "Rezultatai issaugoti i outputs/words.txt" << endl;
     return 0;
 }
